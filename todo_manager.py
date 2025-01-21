f8621a2a9bc0b212c5e86079323a01112564d28b
Abdi Hassan <abdihassan@Abdis-MacBook-Air.local>
2025-01-21 15:00:27 +0100

    changes

--- todo_manager.py
+++ todo_manager.py
@@ -1,6 +1,7 @@
 import sys
 import datetime
 
+
 def helper():
     """
     Help function.
@@ -11,15 +12,16 @@ def helper():
     $ ./todo show             # Show remaining todos
     $ ./todo del NUMBER       # Delete a todo
     $ ./todo done NUMBER      # Complete a todo
+    $ ./todo sort [order]     # Sort todos (alphabetical or date_created)
     $ ./todo helper           # Show usage"""
     sys.stdout.buffer.write(sa.encode('utf8'))
 
 
 def add(todo: str):
     """
-    function to add new todo to existing todo.txt file
+    Function to add a new todo to the existing todo.txt file.
     Args:
-    todo (str): Description of a new todo
+    todo (str): Description of a new todo.
     """
     todo_file = open('todo.txt', 'a')
     todo_file.write(todo)
@@ -30,13 +32,13 @@ def add(todo: str):
 
 def delete(todo_number):
     """
-    function to delete items from the todo list
+    Function to delete items from the todo list.
     Args:
-    todo_number : todo id, which will be deleted
+    todo_number : todo id, which will be deleted.
     """
     try:
         enumerate_todolist()
-        with open("todo.txt", "r") as todo_file:
+        with open("todo.txt", "r+") as todo_file:
             lines = todo_file.readlines()
             todo_file.seek(0)
             for line in lines:
@@ -51,17 +53,15 @@ def delete(todo_number):
 
 def done(todo_number):
     """
-    function to mark todos as done on the todo list
+    Function to mark todos as done on the todo list.
     Args:
-    todo_number : todo id, which will be marked as done
+    todo_number : todo id, which will be marked as done.
     """
     try:
         enumerate_todolist()
-        done_file = open('done.txt', 'a')
-        done_todo_entry = 'x '+str(datetime.datetime.today()).split()[0]+' '+todo_list_dict[int(todo_number)]
-        done_file.write(done_todo_entry)
-        done_file.write("\n")
-        done_file.close()
+        with open('done.txt', 'a') as done_file:
+            done_todo_entry = 'x '+str(datetime.datetime.today()).split()[0]+' '+todo_list_dict[int(todo_number)]
+            done_file.write(done_todo_entry + "\n")
         print(f"Marked todo #{todo_number} as done.")
 
         # delete done todo from todo list
@@ -73,7 +73,7 @@ def done(todo_number):
 
 def show():
     """
-    Function to output current todo list
+    Function to output current todo list.
     """
     try:
         enumerate_todolist()
@@ -95,11 +95,44 @@ def enumerate_todolist():
         todo_file = open('todo.txt', 'r')
         line_counter = 1
         for line in todo_file:
-            line = line.sprip('\n')
+            line = line.strip('\n')
             todo_list_dict.update({line_counter: line})
             line_counter += 1
     except:
         sys.stdout.buffer.write("There are no pending todos! :)".encode('utf8'))
+
+
+def sort_todos(order="alphabetical"):
+    """
+    Function to sort the todo list based on the specified order.
+    Args:
+    order (str): The sorting order - "alphabetical" or "date_created".
+    """
+    try:
+        with open("todo.txt", "r") as todo_file:
+            todos = [line.strip() for line in todo_file.readlines()]
+
+        if not todos:
+            print("There are no todos to sort!")
+            return
+
+        if order == "alphabetical":
+            todos.sort()
+        elif order == "date_created":
+            todos.sort(key=lambda x: x.split()[0])  # Adjust for your date format
+        else:
+            print("Invalid sorting order. Use 'alphabetical' or 'date_created'.")
+            return
+
+        with open("todo.txt", "w") as todo_file:
+            for todo in todos:
+                todo_file.write(todo + "\n")
+
+        print(f"Todos sorted by {order} successfully!")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 if __name__ == '__main__':
     try:
@@ -109,19 +142,22 @@ if __name__ == '__main__':
 
         # change argument for delete for simple function call
         if (args[1] == 'del'):
-            args[1] == 'delete'
+            args[1] = 'delete'
         
         if (args[1] == 'add' and len(args[2:]) == 0):
-            sys.stdout.buffer.write("Error: Missing content of todo. Nothing added.")
+            sys.stdout.buffer.write("Error: Missing content of todo. Nothing added.".encode('utf8'))
 
         elif (args[1] == 'del' and len(args[2:]) == 0):
-            sys.stdout.buffer.write("Error: Todo number is missing. No todo deletet.")
+            sys.stdout.buffer.write("Error: Todo number is missing. No todo deleted.".encode('utf8'))
         
         elif (args[1] == 'done' and len(args[2:]) == 0):
-            sys.stdout.buffer.write("Error: Todo number is missing. No todo marked is done.")
+            sys.stdout.buffer.write("Error: Todo number is missing. No todo marked as done.".encode('utf8'))
+        elif (args[1] == 'sort' and len(args[2:]) == 0):
+            sort_todos()
+        elif (args[1] == 'sort' and len(args[2:]) > 0):
+            sort_todos(*args[2:])
         else:
             globals()[args[1]](*args[2:])
 
     except Exception as e:
         helper()
-        
